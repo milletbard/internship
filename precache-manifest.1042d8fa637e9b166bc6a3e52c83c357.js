5334be365bea30869c5bc4600d43263d54fdeafd
LukeLin <milletbard@gmail.com>
2018-12-23 12:37:57 +0800

    ADD

---
+++ precache-manifest.1042d8fa637e9b166bc6a3e52c83c357.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb8c73c070cf60926c3dd4e4476c8c14",
+    "url": "/Internship/static/media/LukeLin.bb8c73c0.JPG"
+  },
+  {
+    "revision": "5b4b98c660945a9f354613ce1aaf3502",
+    "url": "/Internship/static/media/Home.5b4b98c6.jpg"
+  },
+  {
+    "revision": "5b4b98c660945a9f354613ce1aaf3502",
+    "url": "/Internship/static/media/Footer.5b4b98c6.jpg"
+  },
+  {
+    "revision": "9fb011e4448e706f8f26",
+    "url": "/Internship/static/js/runtime~main.9fb011e4.js"
+  },
+  {
+    "revision": "688985bfefb881e2090b",
+    "url": "/Internship/static/js/main.688985bf.chunk.js"
+  },
+  {
+    "revision": "d3c8ee46ac72224b4771",
+    "url": "/Internship/static/js/1.d3c8ee46.chunk.js"
+  },
+  {
+    "revision": "688985bfefb881e2090b",
+    "url": "/Internship/static/css/main.737f7909.chunk.css"
+  },
+  {
+    "revision": "d3c8ee46ac72224b4771",
+    "url": "/Internship/static/css/1.c40cbfc2.chunk.css"
+  },
+  {
+    "revision": "8324868c62cbc9bceeca533b7e4a09ca",
+    "url": "/Internship/index.html"
+  }
+];
